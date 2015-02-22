17c9c39168a5a8e74ba6a572153e2e6797d5f5c0
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 18:52:53 +0200

    add PrintSequence

---
+++ PrintSequence/PrintSequence/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PrintSequence
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Sequence sequence = new Sequence();
+
+            sequence.QueueTest();
+        }
+    }
+}
---
+++ PrintSequence/PrintSequence/Sequence.cs
@@ -0,0 +1,37 @@
+﻿namespace PrintSequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Sequence
+    {
+        private Queue<int> elements = new Queue<int>();
+        private int elementsCount = 1;
+        private int n = 2;
+        private int current;
+
+        public void QueueTest()
+        {
+            elements.Enqueue(n);
+            Console.Write(elements.Peek() + " ");
+
+            do
+            {
+                current = elements.Dequeue();
+
+                elements.Enqueue(current + 1);
+                Console.Write((current + 1) + " ");
+
+                elements.Enqueue(2 * current + 1);
+                Console.Write((2 * current + 1) + " ");
+
+                elements.Enqueue(current + 2);
+                Console.Write((current + 2) + " ");
+
+
+                elementsCount += 3;
+            } while (elementsCount < 50);
+            Console.WriteLine();
+        }
+    }
+}
