2244d68408058b9559d957301d735e24b85a3b73
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 18:25:02 +0200

    add RemoveOddAppearances

---
+++ RemoveOddAppearances/RemoveOddAppearances/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RemoveOddAppearances
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 3, 3 };
+            Remove remove = new Remove();
+
+            remove.RemoveOddAppearances(numbers);
+
+            foreach (var number in numbers)
+	        {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ RemoveOddAppearances/RemoveOddAppearances/Remove.cs
@@ -0,0 +1,33 @@
+﻿namespace RemoveOddAppearances
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Remove
+    {
+        public List<int> numbersWorkCopy = new List<int>();
+
+        public void RemoveOddAppearances(List<int> numbers)
+        {
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                numbersWorkCopy.Add(numbers[i]);
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        if (numbers[i] == numbers[j])
+                        {
+                            numbersWorkCopy.Add(numbers[j]);
+                        }
+                    }
+                }
+                if (numbersWorkCopy.Count % 2 != 0)
+                {
+                    numbers.RemoveAll(item => item == numbersWorkCopy[0]);
+                }
+                numbersWorkCopy.Clear();
+            }
+        }
+    }
+}
