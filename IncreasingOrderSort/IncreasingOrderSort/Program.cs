379efb97b9e036ff8bed8dc497cd13c5926193eb
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 17:53:58 +0200

    add IncreasingOrderSort

---
+++ IncreasingOrderSort/IncreasingOrderSort/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace IncreasingOrderSort
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Sort numbers = new Sort();
+
+            numbers.GetNumbers();
+            numbers.SortAscending();
+            numbers.PrintNumbers();
+        }
+    }
+}
---
+++ IncreasingOrderSort/IncreasingOrderSort/Sort.cs
@@ -0,0 +1,40 @@
+﻿namespace IncreasingOrderSort
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Sort
+    {
+        private List<int> numbers = new List<int>();
+
+        public void GetNumbers()
+        {
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                if (input != String.Empty)
+                {
+                    numbers.Add(int.Parse(input));
+                }
+                else
+                {
+                    return;
+                }
+            } while (true);
+        }
+
+        public void SortAscending()
+        {
+            numbers.Sort();
+        }
+
+        public void PrintNumbers()
+        {
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
