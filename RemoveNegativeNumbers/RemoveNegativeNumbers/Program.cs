20471c025b49b4518d9b14e5825e2e6915314d7f
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 18:20:16 +0200

    add RemoveNegativeNumbers

---
+++ RemoveNegativeNumbers/RemoveNegativeNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace RemoveNegativeNumbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, -2, 2, 3, -3, 4, 4, -5, -5, 5, -5, -5, -3, 4, 4 };
+            RemoveNumbers remove = new RemoveNumbers();
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+
+            remove.RemoveNegativenumbers(numbers);
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ RemoveNegativeNumbers/RemoveNegativeNumbers/Removenumbers.cs
@@ -0,0 +1,20 @@
+﻿namespace RemoveNegativeNumbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RemoveNumbers
+    {
+        public void RemoveNegativenumbers(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i = 0;
+                }
+            }
+        }
+    }
+}
