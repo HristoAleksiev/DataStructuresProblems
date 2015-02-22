d85b7d2d96c62062ec913b51c69ad40f271fef68
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 18:11:49 +0200

    add LongestSubsequence

---
+++ LongestSubsequence/LongestSubsequence/FindLongestSubsequence.cs
@@ -0,0 +1,43 @@
+﻿namespace LongestSubsequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class FindLongestSubsequence
+    {
+        public List<int> ReturnLongestSubsequence(List<int> numbers)
+        {
+            List<int> longestSubseq = new List<int>();
+            ushort longestStreak = ushort.MinValue;
+            ushort currentStreak = 1;
+            int longestStreakValue = 0;
+            
+            //  Main loop; iterates through "numbers" from the second element to the end 
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                // compares each number with its anterior
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentStreak++;
+                    //  checks if the current subsequence is the largest
+                    if (currentStreak > longestStreak)
+                    {
+                        longestStreak = currentStreak;
+                        longestStreakValue = numbers[i];
+                    }
+                }
+                else
+                {
+                    currentStreak = 1;
+                }
+            }
+
+            //  fill the separate List<int> with the largest subsequence
+            for (int i = 0; i < longestStreak; i++)
+            {
+                longestSubseq.Add(longestStreakValue);
+            }
+            return longestSubseq;
+        }
+    }
+}
---
+++ LongestSubsequence/LongestSubsequence/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LongestSubsequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = new List<int>() { 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5, 5, 3, 4, 4 };
+            List<int> longest;
+
+            FindLongestSubsequence numbers = new FindLongestSubsequence();
+
+            longest = numbers.ReturnLongestSubsequence(numbersList);
+
+            foreach (var number in longest)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
