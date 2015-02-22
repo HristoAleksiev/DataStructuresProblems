7a315615917ae0ce373bb14c9dd5adff4a28ca44
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 19:17:37 +0200

    add LinkedQueue

---
+++ LinkedQueue/LinkedQueue/LinkedQueue.cs
@@ -0,0 +1,52 @@
+﻿namespace LinkedQueue
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class LinkedQueueElement<T>
+    {
+        public T value;
+        public LinkedQueueElement<T> Previous { get; set; }
+        public LinkedQueueElement<T> Next { get; set; }
+    }
+
+    public class LinkedQueue<T>
+    {
+        private LinkedQueueElement<T> Head { get; set; }
+        private LinkedQueueElement<T> Tail { get; set; }
+
+        public void Enqueue(T value)
+        {
+            LinkedQueueElement<T> elemenToAdd = new LinkedQueueElement<T>() { value = value };
+
+            if (this.Head == null)
+            {
+                this.Head = elemenToAdd;
+                this.Tail = elemenToAdd;
+            }
+            else
+            {
+                this.Tail.Next = elemenToAdd;
+                elemenToAdd.Previous = this.Tail;
+                this.Tail = elemenToAdd;
+            }
+        }
+
+        public void Dequeue()
+        {
+            this.Head = this.Head.Next;
+        }
+
+        public void Enumerate()
+        {
+            LinkedQueueElement<T> toPrint = this.Head;
+
+            while (toPrint != null)
+            {
+                Console.Write(toPrint.value + " ");
+                toPrint = toPrint.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinkedQueue/LinkedQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LinkedQueue
+{
+    using System;
+    using System.Collections.Generic;
+    using LinkedQueue;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedQueue<int> numbers = new LinkedQueue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Enqueue(i);
+            }
+
+            numbers.Enumerate();
+
+            for (int i = 0; i < 7; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            numbers.Enumerate();
+        }
+    }
+}
