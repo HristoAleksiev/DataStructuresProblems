a200b78c4f08f38b9399157d5d2a039e43bbed10
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 18:35:47 +0200

    add OccurenceCounter

---
+++ OccurenceCounter/OccurenceCounter/Occurences.cs
@@ -0,0 +1,33 @@
+﻿namespace OccurenceCounter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Occurences
+    {
+        private StringBuilder builder = new StringBuilder();
+        private List<int> occurencesList = new List<int>();
+        private int occurences = 1;
+
+        public void FindOccurences(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        occurences++;
+                        numbers.RemoveAt(j);
+                        j--;
+                    }
+                }
+                occurencesList.Add(occurences);
+                occurences = 1;
+                builder.Append("Number " + numbers[i] + " appears " + occurencesList[i] + " times! \n");
+            }
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
---
+++ OccurenceCounter/OccurenceCounter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace OccurenceCounter
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 3, 4, 4, 2, 3, 3, 4, 3, 2, 6, 6, 3, 3, 3, 3, 3 };
+            Occurences occurences = new Occurences();
+
+            occurences.FindOccurences(numbers);
+        }
+    }
+}
