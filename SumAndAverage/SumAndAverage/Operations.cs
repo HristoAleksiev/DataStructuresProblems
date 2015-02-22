18ca23f465a512dedcb58d56fc002ef82f42ed60
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 17:36:15 +0200

    add SumAndAverage Problem

---
+++ SumAndAverage/SumAndAverage/Operations.cs
@@ -0,0 +1,58 @@
+﻿namespace SumAndAverage
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Operations
+    {
+        public List<int> numbers = new List<int>();
+
+        public void ReadNumbers()
+        {
+            do
+            {
+                string input = Console.ReadLine();
+                if (input != String.Empty)
+                {
+                    numbers.Add(int.Parse(input));
+                }
+                else
+                {
+                    return;
+                }
+            } while (true);
+        }
+
+        public int Average()
+        {
+            if (numbers.Count != 0)
+            {
+                return Sum() / numbers.Count;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            string stringReturn = string.Format(
+                "This is the sum of all the numbers you have entered:   " +
+                Sum() +
+                ";\n" + "and this is the average of all of them:   " +
+                Average() + "!");
+            return stringReturn;
+        }
+    }
+}
---
+++ SumAndAverage/SumAndAverage/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SumAndAverage
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operations = new Operations();
+
+            operations.ReadNumbers();
+            Console.WriteLine(operations.ToString());
+
+            //Console.WriteLine(operations.Sum());
+            //Console.WriteLine(operations.Average());
+        }
+    }
+}
