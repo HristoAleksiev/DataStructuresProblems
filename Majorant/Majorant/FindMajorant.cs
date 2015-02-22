b64559f14a55470622f0c116d17c86e0d8efaced
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 18:46:25 +0200

    add Majorant

---
+++ Majorant/Majorant/FindMajorant.cs
@@ -0,0 +1,37 @@
+﻿namespace Majorant
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class FindMajorant
+    {
+        private int occurences = 1;
+        private int majorantOccurences;
+        private int majorant;
+
+        public int FindMajorantOf(List<int> numbers)
+        {
+            majorantOccurences = numbers.Count / 2 + 1;
+            do
+            {
+                for (int i = 1; i < numbers.Count; i++)
+                {
+                    if (numbers[0] == numbers[i])
+                    {
+                        occurences++;
+                        numbers.RemoveAt(i);
+                        i--;
+                    }
+                }
+                if (occurences >= majorantOccurences)
+                {
+                    majorant = numbers[0];
+                }
+                numbers.RemoveAt(0);
+                occurences = 1;
+            } while (numbers.Count != 0);
+
+            return majorant;
+        }
+    }
+}
---
+++ Majorant/Majorant/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Majorant
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 2, 3, 3, 3, 3, 2, 3, 4, 3, 3 };
+            FindMajorant majorant = new FindMajorant();
+            int theMajorant;
+
+            theMajorant = majorant.FindMajorantOf(numbers);
+            Console.WriteLine(theMajorant);
+        }
+    }
+}
