7619a55f12d8e116f0fed7913ecf38d34571fc8a
HristoAleksiev <kadoll555@gmail.com>
2015-02-22 17:43:19 +0200

    add ReverseWithStack

---
+++ ReverseWithStack/ReverseWithStack/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ReverseWithStack
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseIntegers numbers = new ReverseIntegers();
+
+            numbers.ReadNumbers();
+            numbers.PrintStackReversed();
+        }
+    }
+}
---
+++ ReverseWithStack/ReverseWithStack/ReverseIntegers.cs
@@ -0,0 +1,35 @@
+﻿namespace ReverseWithStack
+{
+    using System;
+    using System.Collections.Generic;
+
+    class ReverseIntegers
+    {
+        public Stack<int> numbers = new Stack<int>();
+
+        public void ReadNumbers()
+        {
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                if (input != String.Empty)
+                {
+                    numbers.Push(int.Parse(input));
+                }
+                else
+                {
+                    return;
+                }
+            } while (true);
+        }
+
+        public void PrintStackReversed()
+        {
+            do
+            {
+                Console.WriteLine(numbers.Pop());
+            } while (numbers.Count != 0);
+        }
+    }
+}
